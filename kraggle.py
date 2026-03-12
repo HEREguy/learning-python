507491c82a6b6ea74574e6cfe8141a94f7a32c2f
ronphonics <ronphonics@gmail.com>
2026-03-11 21:39:08 -0700

    Add script to download AI job replacement dataset from Kaggle

---
+++ kraggle.py
@@ -0,0 +1,7 @@
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("dmahajanbe23/ai-job-replacement-and-skill-shift-dataset")
+
+print("Path to dataset files:", path)
